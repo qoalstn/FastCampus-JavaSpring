58c9c846efb83d45a11b8960973aec8d3a8eac44
qoalstn <mousson2020@gmail.com>
2023-07-07 23:12:23 +0900

    메소드

---
+++ src/ApiOverloading.java
@@ -0,0 +1,8 @@
+public class ApiOverloading {
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10.5f);
+        System.out.println('A');
+        System.out.println("Banana");
+    }
+}
